1401febd27b55900373103471212eb4e9f04abdd
plt-linh <lelinhpplt@gmail.com>
2023-11-06 18:48:10 +0700

    first commit

---
+++ db/MySQLConnect.php
@@ -0,0 +1,16 @@
+<?php
+    $server = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $database = 'website';
+
+    $conn = new mysqli($server, $user, $pass, $database);
+    $conn -> set_charset("utf8");
+    //kiểm tra kết nối 
+    if($conn->connect_error){
+        var_dump($connect->connect_error);
+        echo "\n Kết Nối DB thất bại";
+        die();
+    }
+    else echo "kết nối thành công";
+?>
---
+++ products.php
@@ -0,0 +1,41 @@
+<style>
+html{
+	scroll-behavior: smooth;
+}
+.link{
+	padding: 10px 15px;
+	background: transparent;
+	border:#bccfd8 1px solid;
+	border-left:0px;
+	cursor:pointer;
+	color:#607d8b
+}
+.disabled{
+	cursor:not-allowed;
+	color: #bccfd8;
+}
+.current{
+	background: #bccfd8;
+}
+.first{
+	border-left:#bccfd8 1px solid;
+}
+#pagination{
+	margin-top: 20px;
+
+	padding-top: 30px;
+	border-top: #F0F0F0 1px solid;
+}
+#pagination a{
+	text-decoration:none;
+}
+#paginationWrapper{
+	width:100%;
+	text-align:center
+}
+.dotSign{
+	padding:10px 13px;
+	background:none;
+	border-right: #bccfd8 1px solid;
+}
+</style>
